404628b95813140ecaf28bd404a06f0f48aae014
medabra <abrache2001@gmail.com>
2023-02-25 20:01:08 +0100

    adding databse

---
+++ database/migrations/2023_02_25_183926_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->foreignId('technician_id')->constrained('technicians')->onDelete('cascade');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->decimal('cost', 8, 2);
+            $table->enum('status', ['requested', 'confirmed', 'completed', 'cancelled'])->default('requested');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
